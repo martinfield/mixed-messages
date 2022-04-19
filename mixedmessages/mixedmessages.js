5f538c9ce0339e67b3bc8246669cae51f90fce09
martinfield <madeinarcade@gmail.com>
2022-04-19 12:15:02 -0700

    complete arrays

--- mixedmessages/mixedmessages.js
+++ mixedmessages/mixedmessages.js
@@ -1,3 +1,4 @@
-const myName = "Martin";
-const yourName = '';
-const anotherName = "Greg"
+const character = ['Gandalf', 'Spiderman', 'Catwoman', 'Arya Stark', 'Voldemort']
+const doing = ['Looked across at their rival','pointed their weapon','struck a heroic pose', 'leaped into the air', 'snarled']
+const saying = ['shouted', 'whispered', 'cried', 'moaned', 'mumbled']
+const quote = ["You shall not pass!", "I missed the part where that", "Little birds should stay away from big bad kittycats", "I know Death..He's got many faces. I look forward to seeing this one.", "Avada Kedavra!!!"]
