3c801586365de819e52399cc1d42308df5596e64
martinfield <madeinarcade@gmail.com>
2022-04-19 15:12:27 -0700

    complete arrays in local scope instead of global scope

--- mixedmessages/mixedmessages.js
+++ mixedmessages/mixedmessages.js
@@ -1,17 +1,15 @@
-const characters = ['Gandalf', 'Spiderman', 'Catwoman', 'Arya Stark', 'Voldemort']
-const actionVerbs = ['looked across at their rival','pointed their weapon','struck a heroic pose', 'leaped into the air', 'floated over hauntingly']
-const spokenVerbs = ['shouted', 'whispered', 'cried', 'moaned', 'mumbled']
-const quotes = ["You shall not pass!", "I missed the part where that is my problem.", "Little birds should stay away from big bad kittycats.", "I know Death..He's got many faces. I look forward to seeing this one.", "Avada Kedavra!!!"]
-
 const randomIndex = array => {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-let character = randomIndex(characters);
-let actionVerb = randomIndex(actionVerbs);
-let spokenVerb = randomIndex(spokenVerbs);
-let quote = randomIndex(quotes)
+const mixedMessages = {
+    characters: ['Gandalf', 'Spiderman', 'Catwoman', 'Arya Stark', 'Voldemort', 'Iron Man'],
+    actionVerbs: ['looked across at their rival','pointed their weapon','struck a heroic pose', 'leaped into the air', 'floated over hauntingly', 'suited up'],
+    spokenVerbs: ['shouted', 'whispered', 'cried', 'moaned', 'mumbled'],
+    quotes: ["You shall not pass!", "I missed the part where that is my problem.", "Little birds should stay away from big bad kittycats.", "I know Death..He's got many faces. I look forward to seeing this one.", "Avada Kedavra!!!"],
+    finalMessage() {
+        return `${randomIndex(mixedMessages.characters)} ${randomIndex(mixedMessages.actionVerbs)} and ${randomIndex(mixedMessages.spokenVerbs)} "${randomIndex(mixedMessages.quotes)}"`;
+    }
+}
 
-const mixedMessage = `${character} ${actionVerb} and ${spokenVerb} "${quote}"`;
-
-console.log(mixedMessage);
+console.log(mixedMessages.finalMessage());
