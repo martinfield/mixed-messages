15f42d8f8354be7a06386dcd5a0ea8dbce78df97
martinfield <madeinarcade@gmail.com>
2022-04-19 15:30:58 -0700

    complete all functionality inside object

--- mixedmessages/mixedmessages.js
+++ mixedmessages/mixedmessages.js
@@ -1,19 +1,13 @@
-const randomIndex = array => {
-    return array[Math.floor(Math.random() * array.length)];
-}
-
 const mixedMessages = {
     characters: ['Gandalf', 'Spiderman', 'Catwoman', 'Arya Stark', 'Voldemort', 'Iron Man', 'Mario'],
     actionVerbs: ['looked across at their rival','pointed their weapon','struck a heroic pose', 'leaped into the air', 'floated over hauntingly', 'suited up'],
     spokenVerbs: ['shouted', 'whispered', 'cried', 'moaned', 'mumbled'],
     quotes: ["You shall not pass!", "I missed the part where that is my problem.", "Little birds should stay away from big bad kittycats.", "I know Death..He's got many faces. I look forward to seeing this one.", "Avada Kedavra!!!", "Its a-me!"],
+    randomIndex(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    },
     finalMessage() {
-        return `${randomIndex(mixedMessages.characters)} ${randomIndex(mixedMessages.actionVerbs)} and ${randomIndex(mixedMessages.spokenVerbs)} "${randomIndex(mixedMessages.quotes)}"`;
+        return `${this.randomIndex(mixedMessages.characters)} ${this.randomIndex(mixedMessages.actionVerbs)} and ${this.randomIndex(mixedMessages.spokenVerbs)} "${this.randomIndex(mixedMessages.quotes)}"`;
     }
 }
-
-const mixedMessage = () => {
-    return mixedMessages.finalMessage();
-}
-
-console.log(mixedMessage());
+console.log(mixedMessages.finalMessage());
